bbc75d070d81eda304239bb30f48877486264948
Elliott Tuan <gbs.tuan@gmail.com>
2024-02-24 09:14:12 +0800

    import prisma in getCurrentUser

--- actions/getCurrentUser.ts
+++ actions/getCurrentUser.ts
@@ -1,5 +1,7 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "../pages/api/auth/[...nextauth]";
+import prisma from "@/libs/prismadb"
+
 
 export async function getSession() {
     return await getServerSession(authOptions)
@@ -10,7 +12,7 @@ export async function getCurrentUser() {
         if (!session?.user?.email) {
             return null
         }
-        const currentUser = await prisma?.user.findUnique({
+        const currentUser = await prisma.user.findUnique({
             where: {
                 email: session?.user?.email
             }, include: {
